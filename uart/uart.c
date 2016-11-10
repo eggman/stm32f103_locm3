93e785b5d55953f002cdeebe25263174d0ec74e1
eggman <skouhei@gmail.com>
2016-11-10 22:32:16 +0900

    change code style

--- uart/uart.c
+++ uart/uart.c
@@ -16,19 +16,18 @@ static void clock_setup(void)
 
 static void usart_setup(void)
 {
-  /* Setup A2 pin GPIO_USART2_TX. */
-  gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
-          GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
+  /* Setup GPIO_USART2_TX (A2) pin. */
+  gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
 
-  /* Setup UART parameters. */
+  /* Setup USART parameters. 38400 8 N 1 NOFLOW TXONLY */
   usart_set_baudrate(USART2, 38400);
   usart_set_databits(USART2, 8);
-  usart_set_stopbits(USART2, USART_STOPBITS_1);
-  usart_set_mode(USART2, USART_MODE_TX);
   usart_set_parity(USART2, USART_PARITY_NONE);
+  usart_set_stopbits(USART2, USART_STOPBITS_1);
   usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+  usart_set_mode(USART2, USART_MODE_TX);
 
-  /* Finally enable the USART. */
+  /* Enable USART2. */
   usart_enable(USART2);
 }
 
@@ -40,14 +39,15 @@ int main(void)
   usart_setup();
 
   while (1) {
-    usart_send_blocking(USART2, c + '0');  /* USART2: Send byte. */
+    usart_send_blocking(USART2, c + '0');
     c = (c == 9) ? 0 : c + 1;  /* Increment c. */
-    if ((j++ % 80) == 0) {  /* Newline after line full. */
+    if ((j++ % 80) == 0) {     /* Newline after line full. */
       usart_send_blocking(USART2, '\r');
       usart_send_blocking(USART2, '\n');
     }
-    for (i = 0; i < 800000; i++)  /* Wait a bit. */
+    for (i = 0; i < 800000; i++) {  /* Wait a bit. */
       __asm__("nop");
+    }
   }
 
   return 0;
